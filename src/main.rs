3f38842884258ca4ee7114cecc544c31099dbf4c
ky0he1 <kyouta1218@gmail.com>
2023-04-20 13:13:50 +0900

    Add + and -.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,45 @@
+extern crate ruricho;
+use ruricho::strtol;
+
 use std::env;
 
 fn main() {
     let mut args = env::args();
     if args.len() != 2 {
-        eprint!("Usage: 9cc <code>\n");
+        eprint!("Usage: ruricho <code>\n");
         return;
     }
+
+    let p = args.nth(1).unwrap();
 
     print!(".intel_syntax noprefix\n");
     print!(".global main\n");
     print!("main:\n");
-    print!("  mov rax, {}\n", args.nth(1).unwrap());
+
+    let (n, mut p) = strtol(&p);
+    print!("  mov rax, {}\n", n.unwrap());
+
+    while let Some(c) = p.chars().nth(0) {
+        let s = p.split_off(1);
+
+        if c == '+' {
+            let (n, remaining) = strtol(&s);
+            p = remaining;
+            print!("  add rax, {}\n", n.unwrap());
+            continue;
+        }
+
+        if c == '-' {
+            let (n, remaining) = strtol(&s);
+            p = remaining;
+            print!("  sub rax, {}\n", n.unwrap());
+            continue;
+        }
+
+        eprint!("unexpected character: {}\n", p);
+        return;
+    }
+
     print!("  ret\n");
     return;
 }
